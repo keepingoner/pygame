93714ae33ff0a1f6fcc30ce22732855987c548f1
keepingoner <keepingoner@163.com>
2017-12-25 12:03:31 +0800

    创建项目

---
+++ demo12/ASCII Table.py
@@ -0,0 +1,25 @@
+# ASCII Table Printer
+
+cols = 8
+rows = 256//cols
+table = list("" for n in range(rows+1))
+char = 0
+
+#create strings filled with table data
+for col in range(1,cols+1):
+    for row in range(1,rows+1):
+        table[row] += '{:3.0f}'.format(char) + ' '
+        if char not in (9,10,13): 
+            table[row] += chr(char)
+        table[row] += '\t'
+        char += 1
+
+#print the ASCII table
+for row in table:  print(row)
+
+
+
+   
+                                                        
+
+
